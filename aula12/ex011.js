91c33fcc4413f19b28578619f0492533fd3b0751
eusouoewe <ewe.greed25@gmail.com>
2022-01-26 12:06:37 -0300

    Condições Múltiplas

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 19
+if (idade < 16) {
+        console.log("NÃO VOTA")
+}
+else if (idade < 18 || idade > 65) {
+        console.log ("VOTO OPCIONAL")
+}
+else {
+        console.log ("VOTO OBRIGATÓRIO")
+}
