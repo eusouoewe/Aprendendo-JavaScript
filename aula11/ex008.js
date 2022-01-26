fd4e69c603712a462e8c1d7848c013b29ccb39c9
eusouoewe <ewe.greed25@gmail.com>
2022-01-26 09:10:04 -0300

    Condições

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+        console.log('Você ultrapassou o limite de velocidade permitida. MULTADO!')
+}
+else {
+        console.log('Dirija sempre usando o cinto de segurança!')
+}
