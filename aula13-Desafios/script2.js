236c5a690c43fd01e9449e7176a3c0a80ecf9811
eusouoewe <ewe.greed25@gmail.com>
2022-01-27 18:20:04 -0300

    Desafio 2
    Inicio do desafio 2

---
+++ aula13-Desafios/script2.js
@@ -0,0 +1,25 @@
+function verificar() {
+        var data = new Date()
+        var ano = data.getFullYear()
+        var fAno = document.getElementById('ano')
+        var res = document.getElementById('res')
+        if (fAno.value.length == 0 || Number(fAno.value) > ano ) {
+                window.alert('[ERRO] Verifique os dados e tente novamente.')
+        }
+       else {
+        var fSex = document.getElementsByName('sexo')
+        var idade = ano - Number(fAno.value)
+        var genero = ' '
+        if (fSex[0].checked) {
+                genero = 'Homem'
+                if (fAno.value.length >= 1 && fAno.value.length <= 3) {
+                        
+                }
+        }
+        else if (fSex[1].checked) {
+                genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       }
+}
