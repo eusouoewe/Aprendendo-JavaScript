9c5518a06075ee0d040fbfe02000ee3bb2a85ac2
eusouoewe <ewe.greed25@gmail.com>
2022-01-28 09:15:32 -0300

    Conclusão do Desafio 2

--- aula13-Desafios/script2.js
+++ aula13-Desafios/script2.js
@@ -10,16 +10,56 @@ function verificar() {
         var fSex = document.getElementsByName('sexo')
         var idade = ano - Number(fAno.value)
         var genero = ' '
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fSex[0].checked) {
                 genero = 'Homem'
-                if (fAno.value.length >= 1 && fAno.value.length <= 3) {
-                        
+                if (idade >= 0 && idade == 3) {
+                        //bebe
+                        img.setAttribute('src', 'bebe-menino.jpeg')
+                }
+                else if (idade >= 4 && idade <= 10) {
+                        //menino
+                        img.setAttribute('src', 'menino-pequeno.jpg')
+                }
+                else if (idade >= 11 && idade <= 20) {
+                        //garoto
+                        img.setAttribute('src', 'garoto.jpeg')
+                }
+                else if (idade >= 21 && idade <= 50) {
+                        //adulto
+                        img.setAttribute('src', 'homem.jpeg')
+                }
+                else {
+                        //idoso
+                        img.setAttribute('src', 'idoso.jpeg')
                 }
         }
         else if (fSex[1].checked) {
                 genero = 'Mulher'
+                if (idade >= 0 && idade <= 3) {
+                        //bebe
+                        img.setAttribute('src', 'bebe-menina.jpeg')
+                }
+                else if (idade >= 4 && idade <= 10) {
+                        //menina
+                        img.setAttribute('src', 'menina-pequena.jpg')
+                }
+                else if (idade >= 11 && idade <= 20) {
+                        //garota
+                        img.setAttribute('src', 'garota.jpeg')
+                }
+                else if (idade >= 21 && idade <= 50) {
+                        //adulta
+                        img.setAttribute('src', 'mulher.jpeg')
+                }
+                else {
+                        //idosa
+                        img.setAttribute('src', 'idosa.jpeg')
+                }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
        }
 }
