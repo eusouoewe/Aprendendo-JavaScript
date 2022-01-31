f012676fee9c04dd82a3f0ee12be99e51216d16d
eusouoewe <ewe.greed25@gmail.com>
2022-01-31 17:27:45 -0300

    Inicio de mais desafios

---
+++ aula14-Desafios/desafio1/script1.js
@@ -0,0 +1,6 @@
+function contar() {
+        var inicio = document.getElementById('inicio');
+        var fim = document.getElementById('fim');
+        var passo = document.getElementById('passo');
+        var res = document.getElementById('res');
+}
