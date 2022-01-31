e0a4b2dee43a3c06e0b80bd3b97e2fcbdffe628b
eusouoewe <ewe.greed25@gmail.com>
2022-01-31 11:56:43 -0300

    Aprendendo estruturas de repetição

---
+++ aula14/ambiente.js
@@ -0,0 +1,9 @@
+ var cont = 1
+ while (cont <= 10) {
+         console.log(`${cont}`)
+         cont ++
+ }
+
+ for (var cont = 1; cont <= 10; cont ++) {
+         console.log(`${cont}`)
+ }
---
+++ aula14/ambiente2.js
@@ -0,0 +1,6 @@
+console.log('Vai começar...')
+console.log('Contando!')
+for (var cont = 1; cont <= 5; cont ++) {
+        console.log(`${cont}`)
+}
+console.log('FIM')
